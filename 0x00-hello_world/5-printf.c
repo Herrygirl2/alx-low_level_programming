90628db57736310d0eb920502a6d64978f1c118e
Herrygirl2 <inioluwailesanmi0@gmail.com>
2022-10-13 06:05:29 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main = main function entry point
+ *
+ * Always return 0 on success
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return 0;
+}
