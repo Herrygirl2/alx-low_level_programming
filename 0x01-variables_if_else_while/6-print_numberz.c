30f0904919e6a4641c2be13e6d282466249c8466
Herrygirl2 <inioluwailesanmi0@gmail.com>
2022-10-14 12:14:29 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int k;
+
+for (k = 0; k <= 10; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
