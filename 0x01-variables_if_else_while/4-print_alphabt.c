e67eba5fded6dcb1ee81470313bcddfc0a1f1d41
Herrygirl2 <inioluwailesanmi0@gmail.com>
2022-10-14 11:41:45 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function is the entry point
+ * Return: Always return 0 on success
+ */
+int main(void)
+{
+	char a;
+	for (a='a'; a<='z'; a++)
+{
+		if (a!='q' && a!='e')
+{
+		putchar(a);
+}
+} 	
+	putchar('\n');
+	return (0);
+}
