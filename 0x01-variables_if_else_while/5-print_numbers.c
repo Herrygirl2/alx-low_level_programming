354735f12667cda7fe49a579db49debba7ca18b9
Herrygirl2 <inioluwailesanmi0@gmail.com>
2022-10-14 12:04:53 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: random number in n +ve/-ve
+ *Return: 0 on success
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
