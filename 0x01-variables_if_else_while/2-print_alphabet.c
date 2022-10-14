f873ff1c5804436e5b0420351b42c8a730202974
Herrygirl2 <inioluwailesanmi0@gmail.com>
2022-10-14 11:28:52 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function is the entry point
+ * Return: Always return 0 on success
+ */
+int main(void)
+{
+	int a;
+	for (a=97; a<=27; a++)
+{
+		putchar(a);
+	putchar(10);
+} 
+	
+	return (0);
+}
